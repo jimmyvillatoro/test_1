a21bfda6da2619b4abd4f136a6107638d0605f8e
Jimmy Villatoro <48574266+jimmyvillatoro@users.noreply.github.com>
2022-03-03 11:47:32 -0600

    Add files via upload

---
+++ dat/cdb/db.php
@@ -0,0 +1,13 @@
+<?php 
+$servidor  ="localhost"; 
+$usuario   ="c2sql11145co_eu";  
+$clave     ="goYidZD!P2";
+$basedatos ="c2sql11145co_eu";
+
+$db_connection = mysqli_connect($servidor, $usuario, $clave, $basedatos) or die(mysql_error());
+
+if (!$db_connection) {
+    die('No se ha podido conectar a la base de datos');
+}else
+    echo mysqli_connect_error($db_connection);
+?>
